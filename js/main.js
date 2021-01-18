5e13f8e0e50b609689dc6b54f671283bac780042
anjiae <74403773+anjiae@users.noreply.github.com>
2021-01-18 16:14:00 +0900

    +javascript

--- js/main.js
+++ js/main.js
@@ -10,4 +10,60 @@ window.addEventListener('scroll', ()=> {
   }
 });
 
+//Handle navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', event => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link ===null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: "smooth"});
+}
+
+//Show "arrow up" button
+const home = document.querySelector('#home');
+const arrowUp = document.querySelector('.arrow-up');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+//handle "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView("#home");
+});
+
+
+//Services scroll event
+function serviceBottom(ele, differ) {
+  const {top} = ele.getBoundingClientRect();
+  const {innerHeight} = window;
+  return top > innerHeight + (differ || 0);
+}
+
+function scrollService() {
+  const elems = document.querySelectorAll('.up-service');
+  elems.forEach(list => {
+    if(serviceBottom(list, -250)) {
+      list.style.opacity = "0";
+      list.style.transform = "translateY(100px)";
+    }else {
+      list.style.opacity = "1";
+      list.style.transform = "translateY(0px)";
+    }
+  });
+}
+
+window.addEventListener('scroll', scrollService);
 
